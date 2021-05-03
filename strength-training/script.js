a56762c0b6dab8148c00953c928e0a3c9ad11b55
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-05-03 11:07:37 -0700

    Add files via upload

--- strength-training/script.js
+++ strength-training/script.js
@@ -1,10 +1,9 @@
 const slides = ["title", "section_one", "section_two", "section_three"];
-
 const loc = location.href.split("/");
 const slide = slides.indexOf(loc[loc.length - 1].replace(/\.html/, ""));
 document.onclick = (t) => {
     if (t.pageX > window.innerWidth / 2) {
-        if (slides.indexOf(loc[loc.length - 1].replace(/\.html/, "")) < slides.length) {
+        if (slides.indexOf(loc[loc.length - 1].replace(/\.html/, "")) < slides.length - 1) {
             location.href = slides[slide + 1] + ".html";
         }
     } else {
@@ -12,10 +11,9 @@ document.onclick = (t) => {
             location.href = slides[slide - 1] + ".html";
         }
     }
-    //location.href = loc.join("/") + ".html";
 }
 
-document.title = "Slide " + loc[loc.length - 1];
+document.title = "Slide - " + slides[slide];
 
 const icon = document.createElement("link");
 icon.rel = "icon";
