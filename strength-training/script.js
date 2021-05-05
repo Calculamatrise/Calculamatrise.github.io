c520b295947c3c73a227e6a56eddbc9fb94278f1
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-05-05 09:58:35 -0700

    Update script.js

--- strength-training/script.js
+++ strength-training/script.js
@@ -1,19 +1,19 @@
-const slides = ["title", "section_one", "section_two", "section_three"];
+const slides = ["", "section_one.html", "section_two.html", "section_three.html"];
 const loc = location.href.split("/");
 const slide = slides.indexOf(loc[loc.length - 1].replace(/\.html/, ""));
 document.onclick = (t) => {
     if (t.pageX > window.innerWidth / 2) {
         if (slides.indexOf(loc[loc.length - 1].replace(/\.html/, "")) < slides.length - 1) {
-            location.href = slides[slide + 1] + ".html";
+            location.href = slides[slide + 1];
         }
     } else {
         if (slides.indexOf(loc[loc.length - 1].replace(/\.html/, "")) > 0) {
-            location.href = slides[slide - 1] + ".html";
+            location.href = slides[slide - 1];
         }
     }
 }
 
-document.title = "Slide - " + slides[slide];
+document.title = "Strength Training - " + slides[slide].replace(/_/, " ").replace(/\.html/, "");
 
 const icon = document.createElement("link");
 icon.rel = "icon";
