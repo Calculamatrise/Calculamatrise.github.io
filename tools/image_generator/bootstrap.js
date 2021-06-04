6e64b43fbed00f28697687c37453cee7c6b4a454
Ayoob Tahar <tahar.ayoob@gmail.com>
2021-06-03 17:20:15 -0700

    Improved script

--- tools/image_generator/bootstrap.js
+++ tools/image_generator/bootstrap.js
@@ -5,64 +5,54 @@ let scenery = new Array();
 
 const reader = new FileReader();
 reader.onload = function() {
-    image.src = this.result;
-}
-
-const image = new Image();
-image.onload = function() {
-    canvas.width = this.width;
-    canvas.height = this.height;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
-
     code.value = null;
     
     solid = new Array();
     scenery = new Array();
 
+    image.src = this.result;
+}
+
+const image = new Image();
+image.onload = function() {
+    canvas.width = size.checked ? this.width : 300;
+    canvas.height = size.checked ? this.height : 300;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+
 	const pixelData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-	for (let t = 0; t in pixelData.data; t += 4) {
+	for (let t = 0, x = 0, y = 0; t in pixelData.data; t += 4, x++) {
         const average = (pixelData.data[t] + pixelData.data[t + 1] + pixelData.data[t + 2]) / 3;
 		const bw = pixelData.data[t] * .2 + pixelData.data[t + 1] * .7 + pixelData.data[t + 2] * .1;
-		
-		pixelData.data[t] = pixelData.data[t + 1] = pixelData.data[t + 2] = bw;
+
+        if (x >= canvas.width) x = 0, y++;
+
+        // document.title = "Progress... " + Math.round(t / 3) + "%";
+    
+		pixelData.data[t] = pixelData.data[t + 1] = pixelData.data[t + 2] = bw <= 85 ? 0 : bw <= 170 || bw < 210 && x % 2 == 0 && y % 2 == 0 ? 170 : 255;
+
+        if (pixelData.data[t] > 210) continue;
+
+        const line = new Line({
+            x: x,
+            y: y,
+            dx: x + 2,
+            dy: y + 2
+        });
+        line.filter(pixelData.data[t]);
     }
 
     ctx.putImageData(pixelData, 0, 0);
 
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let y = 0; y < canvas.height; y++) {
-        for (let x = 0; x < canvas.width; x++) {
-            const index = (x + y * canvas.width) * 4;
-            if (index % 2) return;
-            
-            const line = new Line({
-                x: x,
-                y: y,
-                dx: x + 2,
-                dy: y + 2
-            });
-            if (pixelData.data[index] <= 85) {
-                ctx.strokeStyle = "#000";
-                solid.push(line);
-            } else if (pixelData.data[index] <= 170) {
-                ctx.strokeStyle = "#AAA";
-                scenery.push(line);
-            } else if (pixelData.data[index] < 210 && x % 2 == 0 && y % 2 == 0) {
-                ctx.strokeStyle = "#AAA";
-                scenery.push(line);
-            }
-        }
-    }
-
-    code.value = solid.map(t => t.encode.toString()).join(",") + "#" + scenery.map(t => t.encode.toString()).map((t, e, i) => t.dx == i[e + 1]?.x && t.dy == i[e + 1]?.y && (t.dx = i[e + 1]?.dx, t.dy = i[e + 1]?.dy, delete i[e + 1], t)).filter(t => t != null).join(",") + "#";
+    code.value = solid.filter(t => t != null).map(t => t.encode.toString()).join(",") + "#" + scenery.filter(t => t != null).map(t => t.encode.toString()).join(",") + "#";
 }
 image.crossOrigin = "Anonymous";
 
 img.onchange = function() {
+    if (this.files.length < 1) return;
+
     reader.readAsDataURL(this.files[0]);
 }
 
@@ -72,8 +62,6 @@ class Line {
         this.y = y;
         this.dx = dx;
         this.dy = dy;
-
-        // this.draw();
     }
     get encode() {
         this.x = parseInt(this.x).toString(32);
@@ -83,11 +71,16 @@ class Line {
 
         return this;
     }
-    draw() {
-        console.log(this.x, this.y, this.dx, this.dy)
-        // ctx.moveTo(this.x, this.y);
-        // ctx.lineTo(this.dx, this.dy);
-        // ctx.stroke();
+    filter(type) {
+        const t = type ? scenery : solid;
+        const e = t.findIndex(e => e && this.x == e.dx && this.y == e.dy);
+        if (e >= 0) {
+            this.x = t[e].x;
+            this.y = t[e].y;
+            delete t[e];
+        }
+
+        t.push(this);
 
         return this;
     }
