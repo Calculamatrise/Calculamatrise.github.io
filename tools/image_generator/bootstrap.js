d6dfef98193fb2c0bdd9a605d29bc450e94aa2e0
Ayoob Tahar <tahar.ayoob@gmail.com>
2021-06-07 15:02:28 -0700

    Render rewrite.

--- tools/image_generator/bootstrap.js
+++ tools/image_generator/bootstrap.js
@@ -1,6 +1,5 @@
 import Manipulation from "./Utils/Manipulation.js";
 
-        this.solid = new Array();
 image.onchange = function() {
     if (this.files.length < 1) return;
     Manipulation.fileReader.readAsDataURL(this.files[0]);
