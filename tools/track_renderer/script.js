106be798e8e833ec74a08c9c9c1f2bab86211738
Ayoob Tahar <tahar.ayoob@gmail.com>
2021-07-08 22:51:34 -0700

    What am I doing?

--- tools/track_renderer/script.js
+++ tools/track_renderer/script.js
@@ -1,34 +1,41 @@
 const ctx = canvas.getContext("2d");
-ctx.translate(canvas.width / 2, canvas.height / 2);
-run.onclick = () => {
-    fetch("https://cors-anywhere.herokuapp.com/https://cdn.freeriderhd.com/free_rider_hd/tracks/prd/" + input.value + "/track-data-v1.js").then(response => response.text()).then(function(data) {
-        data = JSON.parse(data.slice(2, -2));
-        let code = data.code.split("#");
-        let physics = code[0].split(",").map(t => t.split(/\s/).map(t => parseInt(t, 32)));
-        let scenery = code[1].split(",").map(t => t.split(/\s/).map(t => parseInt(t, 32)));
-        ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-        ctx.lineWidth = 2;
-        ctx.lineCap = "round";
-        ctx.lineJoin = "round";
-        ctx.strokeStyle = invert.checked ? "#555" : "#AAA";
-        for (const t of scenery) {
-            for (let e = 0; e < t.length; e += 2) {
-                ctx.beginPath();
-                ctx.moveTo(t[e], t[e + 1]);
-                ctx.lineTo(t[e + 2], t[e + 3]);
-                ctx.stroke();
-            }
+function t(e) {
+    let code = e.code.split("#");
+    let physics = code[0].split(",").map(t => t.split(/\s/).map(t => parseInt(t, 32)));
+    let scenery = code[1].split(",").map(t => t.split(/\s/).map(t => parseInt(t, 32)));
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = 2;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.moveTo(0, 0);
+    ctx.lineTo(1, 1)
+    let limitX = physics.sort((t, e) => t[0] - e[0]);
+    canvas.width = Math.abs(limitX[0][0]) + Math.abs(limitX[limitX.length - 1][0]);
+    let limitY = physics.sort((t, e) => t[1] - e[1]);
+    canvas.height = Math.abs(limitY[0][1]) + Math.abs(limitY[limitY.length - 1][1]);
+    ctx.translate(limitX[0][0] / 2, limitY[0][0] / 2);
+    ctx.strokeStyle = invert.checked ? "#FFF" : "#000";
+    for (const t of physics) {
+        for (let e = 0; e < t.length; e += 2) {
+            ctx.beginPath();
+            ctx.moveTo(t[e], t[e + 1]);
+            ctx.lineTo(t[e + 2], t[e + 3]);
+            ctx.stroke();
         }
-        ctx.strokeStyle = invert.checked ? "#FFF" : "#000";
-        for (const t of physics) {
-            for (let e = 0; e < t.length; e += 2) {
-                ctx.beginPath();
-                ctx.moveTo(t[e], t[e + 1]);
-                ctx.lineTo(t[e + 2], t[e + 3]);
-                ctx.stroke();
-            }
+    }
+    ctx.strokeStyle = invert.checked ? "#555" : "#AAA";
+    for (const t of scenery) {
+        for (let e = 0; e < t.length; e += 2) {
+            ctx.beginPath();
+            ctx.moveTo(t[e], t[e + 1]);
+            ctx.lineTo(t[e + 2], t[e + 3]);
+            ctx.stroke();
         }
-    }).catch(function(error) {
-        canvas.prepend("Something went wrong. Please try again later. (Too many requests)\n" + error);
-    });
+    }
+}
+run.onclick = function() {
+    document.body.append(Object.assign(document.createElement("script"), {
+        src: "https://cdn.freeriderhd.com/free_rider_hd/tracks/prd/" + input.value + "/track-data-v1.js"
+    }));
 }
