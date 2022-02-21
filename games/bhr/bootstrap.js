0d0835a89a3cbe0e9903fa2b43340b039a8df300
Calculus <tahar.ayoob@gmail.com>
2022-02-20 20:01:28 -0800

    Moved to a separate repository

--- games/bhr/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import Game from "./class/Game.js";
-
-window.game = new Game(document.querySelector("#view"));
-
-localStorage.setItem("theme", localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"));
--- games/bhr/class/item/Gravity.js
+++
@@ -1,9 +0,0 @@
-import Triangle from "./Triangle.js";
-
-export default class Gravity extends Triangle {
-    type = "G";
-    color = "#0f0";
-    activate(part) {
-        part.parent.parent.gravity.copy(this.dir.scale(.3));
-    }
-}
