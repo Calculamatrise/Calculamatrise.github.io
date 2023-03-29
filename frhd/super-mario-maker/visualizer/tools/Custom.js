ba0cd654ebcfcfdf7045c29daaab3e5310f18ebf
Calculus <tahar.ayoob@gmail.com>
2023-03-29 02:46:42 -0700

    Bug fix

--- frhd/super-mario-maker/visualizer/tools/Custom.js
+++ frhd/super-mario-maker/visualizer/tools/Custom.js
@@ -8,10 +8,11 @@ export default class extends ObjectTool {
 			const parts = code.split('#').map(part => part.split(/,+/g).filter(empty => empty).map(line => line.split(/\s+/g).map(coord => parseInt(coord, 32))));
 			this.constructor.physics = parts[0];
 			this.constructor.scenery = parts[1];
-			const flatX = this.constructor.physics.flatMap(lines => lines.filter((_, index) => index % 2 == 0));
-			const width = (Math.abs(Math.min(...flatX)) + Math.abs(Math.max(...flatX)));
-			const flatY = this.constructor.physics.flatMap(lines => lines.filter((_, index) => index % 2));
-			const height = (Math.abs(Math.min(...flatY)) + Math.abs(Math.max(...flatY)))
+			const joined = Array(...this.constructor.physics, ...this.constructor.scenery);
+			const flatX = joined.flatMap(lines => lines.filter((_, index) => index % 2 == 0));
+			const width = Math.abs(Math.min(...flatX)) + Math.abs(Math.max(...flatX));
+			const flatY = joined.flatMap(lines => lines.filter((_, index) => index % 2));
+			const height = Math.abs(Math.min(...flatY)) + Math.abs(Math.max(...flatY));
 			this.offsetX = width / 2;
 			this.offsetY = height / 2;
 		}
