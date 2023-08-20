c08b6e9a80bceab91d59ebb0e5700475cb4915cb
Calculamatrise <tahar.ayoob@gmail.com>
2023-08-20 13:33:51 -0700

    Bug fixes

--- frhd/utils/Track.js
+++ frhd/utils/Track.js
@@ -3,13 +3,13 @@ class Track {
 	scenery = [];
 	powerups = {
 		antigravity: [],
-		bombs: [],
-		boosters: [],
-		checkpoints: [],
+		bomb: [],
+		booster: [],
+		checkpoint: [],
 		gravity: [],
-		slowmos: [],
-		targets: [],
-		teleporters: [],
+		slowmo: [],
+		target: [],
+		teleporter: [],
 		vehicles: {
 			balloon: [],
 			blob: [],
@@ -22,26 +22,17 @@ class Track {
 	}
 
 	clear() {
-		this.physics.splice(0, this.physics.length);
-		this.scenery.splice(0, this.scenery.length);
+		this.physics.splice(0);
+		this.scenery.splice(0);
 		for (const type in this.powerups) {
 			switch (type) {
-				case 'antigravity':
-				case 'bomb':
-				case 'booster':
-				case 'checkpoint':
-				case 'gravity':
-				case 'slomo':
-				case 'target':
-				case 'teleporter':
-					this.powerups[type].splice(0, this.powerups[type].length);
-					break;
-
 				case 'vehicles':
 					for (const vehicle in this.powerups[type]) {
-						this.powerups[type][vehicle].splice(0, this.powerups[type][vehicle]);
+						this.powerups[type][vehicle].splice(0);
 					}
 					break;
+				default:
+					this.powerups[type].splice(0);
 			}
 		}
 	}
@@ -115,26 +106,18 @@ class Track {
 		for (const type in this.powerups) {
 			for (const powerup in this.powerups[type]) {
 				switch (type) {
-					case 'teleporter':
-						this.powerups[type][powerup][2] += x;
-						this.powerups[type][powerup][3] += y;
-					case 'antigravity':
-					case 'bomb':
-					case 'booster':
-					case 'checkpoint':
-					case 'gravity':
-					case 'slomo':
-					case 'target':
-						this.powerups[type][powerup][0] += x;
-						this.powerups[type][powerup][1] += y;
-						break;
-
 					case 'vehicles':
 						for (const vehicle of this.powerups[type][powerup]) {
 							vehicle[0] += x;
 							vehicle[1] += y;
 						}
 						break;
+					case 'teleporter':
+						this.powerups[type][powerup][2] += x;
+						this.powerups[type][powerup][3] += y;
+					default:
+						this.powerups[type][powerup][0] += x;
+						this.powerups[type][powerup][1] += y;
 				}
 			}
 		}
@@ -162,25 +145,6 @@ class Track {
 		for (const type in this.powerups) {
 			for (const powerup in this.powerups[type]) {
 				switch (type) {
-					case 'teleporter':
-						let e = this.powerups[type][powerup][2];
-						this.powerups[type][powerup][2] = Math.cos(x) * e + Math.sin(x) * this.powerups[type][powerup][3],
-						this.powerups[type][powerup][3] = -Math.sin(x) * e + Math.cos(x) * this.powerups[type][powerup][3];
-					case 'antigravity':
-					case 'bomb':
-					case 'booster':
-					case 'checkpoint':
-					case 'gravity':
-					case 'slomo':
-					case 'target':
-						let i = this.powerups[type][powerup][0];
-						this.powerups[type][powerup][0] = Math.cos(x) * i + Math.sin(x) * this.powerups[type][powerup][1],
-						this.powerups[type][powerup][1] = -Math.sin(x) * i + Math.cos(x) * this.powerups[type][powerup][1];
-						if (['booster', 'gravity'].includes(type)) {
-							this.powerups[type][powerup][2] += x / -Math.PI * 180;
-						}
-						break;
-
 					case 'vehicles':
 						for (const vehicle of this.powerups[type][powerup]) {
 							let i = vehicle[0];
@@ -188,6 +152,17 @@ class Track {
 							vehicle[1] = -Math.sin(x) * i + Math.cos(x) * vehicle[1];
 						}
 						break;
+					case 'teleporter':
+						let e = this.powerups[type][powerup][2];
+						this.powerups[type][powerup][2] = Math.cos(x) * e + Math.sin(x) * this.powerups[type][powerup][3],
+						this.powerups[type][powerup][3] = -Math.sin(x) * e + Math.cos(x) * this.powerups[type][powerup][3];
+					default:
+						let i = this.powerups[type][powerup][0];
+						this.powerups[type][powerup][0] = Math.cos(x) * i + Math.sin(x) * this.powerups[type][powerup][1],
+						this.powerups[type][powerup][1] = -Math.sin(x) * i + Math.cos(x) * this.powerups[type][powerup][1];
+						if (['booster', 'gravity'].includes(type)) {
+							this.powerups[type][powerup][2] += x / -Math.PI * 180;
+						}
 				}
 			}
 		}
@@ -215,26 +190,18 @@ class Track {
 		for (const type in this.powerups) {
 			for (const powerup in this.powerups[type]) {
 				switch (type) {
-					case 'teleporter':
-						this.powerups[type][powerup][2] *= x;
-						this.powerups[type][powerup][3] *= y;
-					case 'antigravity':
-					case 'bomb':
-					case 'booster':
-					case 'checkpoint':
-					case 'gravity':
-					case 'slomo':
-					case 'target':
-						this.powerups[type][powerup][0] *= x;
-						this.powerups[type][powerup][1] *= y;
-						break;
-
 					case 'vehicles':
 						for (const vehicle of this.powerups[type][powerup]) {
 							vehicle[0] *= x;
 							vehicle[1] *= y;
 						}
 						break;
+					case 'teleporter':
+						this.powerups[type][powerup][2] *= x;
+						this.powerups[type][powerup][3] *= y;
+					default:
+						this.powerups[type][powerup][0] *= x;
+						this.powerups[type][powerup][1] *= y;
 				}
 			}
 		}
@@ -251,30 +218,19 @@ class Track {
 	toString() {
 		let powerups = [];
 		for (const type in this.powerups) {
-			for (const powerup in this.powerups[type]) {
-				switch (type) {
-					case 'target':
-					case 'booster':
-					case 'gravity':
-					case 'slowmo':
-					case 'bomb':
-					case 'checkpoint':
-					case 'antigravity':
-					case 'teleporter':
-						const id = Object.entries(this.constructor.dict).find(([, value]) => value == type).shift();
-						powerups.push(this.powerups[type][powerup].map(powerup => id + ' ' + powerup.map(t => Math.round(t).toString(32)).join(' ')).join(','));
-						break;
-
-					case 'vehicles':
-						for (const vehicle in this.powerups[type][powerup]) {
-							powerups.push(this.powerups[type][powerup][vehicle].map(powerup => 'V ' + powerup.map(t => Math.round(t).toString(32)).join(' ')).join(','));
-						}
-						break;
-				}
+			switch (type) {
+				case 'vehicles':
+					for (const vehicle in this.powerups[type]) {
+						powerups.push(this.powerups[type][vehicle].map(powerup => 'V ' + powerup.map(t => Math.round(t).toString(32)).join(' ')).join(','));
+					}
+					break;
+				default:
+					const id = Object.entries(this.constructor.dict).find(([, value]) => value == type).shift();
+					powerups.push(this.powerups[type].map(powerup => id + ' ' + powerup.map(t => Math.round(t).toString(32)).join(' ')).join(','));
 			}
 		}
 
-		return Array(this.physics.map(vector => vector.map(t => Math.round(t).toString(32)).join(' ')).join(','), this.scenery.map(vector => vector.map(t => Math.round(t).toString(32)).join(' ')).join(','), powerups.filter(i => i).join(',')).join('#');
+		return Array(this.physics.map(line => line.map(t => Math.round(t).toString(32)).join(' ')).join(','), this.scenery.map(line => line.map(t => Math.round(t).toString(32)).join(' ')).join(','), powerups.filter(i => i).join(',')).join('#');
 	}
 
 	static dict = {
